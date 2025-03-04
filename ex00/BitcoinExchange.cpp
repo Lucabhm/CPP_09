fe160c19006069cdef2b8b089a0d81f5101ab881
Lucabhm <lbohm@student.42heilbronn.de>
2025-03-04 13:55:52 +0100

    fixed segfault in ex00

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: lbohm <lbohm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 23:07:37 by lbohm             #+#    #+#             */
-/*   Updated: 2025/03/04 11:36:44 by lbohm            ###   ########.fr       */
+/*   Updated: 2025/03/04 13:55:27 by lbohm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,8 +64,8 @@ void	BitcoinExchange::checkPrice(void) const
 	{
 		if (checkDate(it->first))
 		{
-			found = this->db.lower_bound(it->first);
-			if (found == this->db.end())
+			found = this->db.upper_bound(it->first);
+			if (found == this->db.end() || (found->first != it->first && found != this->db.begin()))
 				--found;
 			if (checkNbr(it->second))
 				std::cout << it->first << " => " << it->second << " = " << std::stof(it->second) * std::stof(found->second) << std::endl;
